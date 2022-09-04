f08e4178b73a993194005d03335bd7a4c31b508e
Yui Matsuura <teshi04@gmail.com>
2022-09-04 14:35:09 +0900

    Fix items order

--- lib/pages/data/url.dart
+++ lib/pages/data/url.dart
@@ -1,7 +1,7 @@
 final items = [
   {'title': 'Twitter', 'url': 'https://twitter.com/teshi04'},
   {'title': 'GitHub', 'url': 'https://github.com/teshi04'},
+  {'title': 'Scrapbox', 'url': 'https://scrapbox.io/teshi04'},
   {'title': 'note', 'url': 'https://note.com/teshi04'},
   {'title': 'Blog', 'url': 'https://teshi04.hatenablog.com/'},
-  {'title': 'Scrapbox', 'url': 'https://scrapbox.io/teshi04'}
 ];
