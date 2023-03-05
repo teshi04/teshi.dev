bc6ef0241ce329d477e5ec557b1d13a161ba3c79
Yui Matsuura <teshi04@gmail.com>
2023-03-05 14:11:28 +0900

    Remove

--- lib/pages/data/url.dart
+++ lib/pages/data/url.dart
@@ -1,7 +1,6 @@
 final items = [
   {'emoji': '🐔', 'title': 'Twitter', 'url': 'https://twitter.com/teshi04'},
   {'emoji': '🐙', 'title': 'GitHub', 'url': 'https://github.com/teshi04'},
-  {'emoji': '🧰', 'title': 'Scrapbox', 'url': 'https://scrapbox.io/teshi04'},
   {'emoji': '📝', 'title': 'note', 'url': 'https://note.com/teshi04'},
   {'emoji': '👣', 'title': 'Blog', 'url': 'https://teshi04.hatenablog.com/'},
 ];
