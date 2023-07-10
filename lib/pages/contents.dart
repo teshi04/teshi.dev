3498c04e6c6a17e234108610068088321d055fd7
Yui Matsuura <teshi04@gmail.com>
2023-07-11 00:52:56 +0900

    Split files

---
+++ lib/pages/contents.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
+import 'package:teshi_dev/app_color.dart';
+import 'package:teshi_dev/pages/data/url.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Contents extends StatelessWidget {
+  const Contents({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.all(32),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('teshi04', style: textTheme.displayMedium),
+                  Text(
+                    'Yui Matsuura',
+                    style: textTheme.headlineSmall
+                        ?.copyWith(color: Colors.grey[600]),
+                  ),
+                ],
+              ),
+              CircleAvatar(
+                backgroundColor: AppColors.primaryColor,
+                radius: 64,
+                child: SvgPicture.asset(
+                  'assets/nekouo.svg',
+                ),
+              )
+            ],
+          ),
+          const Gap(32),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: items
+                .map(
+                  (item) => Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Tooltip(
+                      message: item['url'],
+                      verticalOffset: 11,
+                      child: FilledButton.tonal(
+                        style: FilledButton.styleFrom(
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(32),
+                          ),
+                          padding: const EdgeInsets.all(16),
+                        ),
+                        onPressed: () async {
+                          await launchUrl(Uri.parse(item['url'] ?? ''));
+                        },
+                        child: Row(
+                          children: [
+                            Text(
+                              item['emoji'] ?? '',
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontFamily: 'NotoColorEmoji',
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            Text(
+                              item['title'] ?? '',
+                              style: textTheme.titleMedium,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+                .toList(),
+          ),
+          const Gap(32),
+          Row(
+            children: [
+              _buildCard(context),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCard(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    const suzuriUrl = 'https://suzuri.jp/teshi04';
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      child: Stack(
+        children: [
+          Tooltip(
+            message: suzuriUrl,
+            child: InkWell(
+              child: Ink.image(
+                width: 160,
+                height: 160,
+                fit: BoxFit.cover,
+                image: const AssetImage('assets/suzuri.png'),
+                child: InkWell(
+                  onTap: () async {
+                    await launchUrl(Uri.parse(suzuriUrl));
+                  },
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(16),
+            child: Text(
+              'ウサ木',
+              style: textTheme.titleMedium?.copyWith(color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/index.dart
+++ lib/pages/index.dart
@@ -1,22 +1,17 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cube/flutter_cube.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:gap/gap.dart';
-import 'package:teshi_dev/app_color.dart';
-import 'package:url_launcher/url_launcher.dart';
-
-import 'data/url.dart';
+import 'package:teshi_dev/pages/contents.dart';
 
 class TopPage extends StatefulWidget {
   const TopPage({Key? key}) : super(key: key);
 
   @override
-  _TopPageState createState() => _TopPageState();
+  TopPageState createState() => TopPageState();
 }
 
-class _TopPageState extends State<TopPage> with SingleTickerProviderStateMixin {
-  final Object _model = Object(
+class TopPageState extends State<TopPage> with SingleTickerProviderStateMixin {
+  final _model = Object(
     position: Vector3(0, -1.0, 0),
     rotation: Vector3(0, -90.0, 0.0),
     scale: Vector3(10.0, 10.0, 10.0),
@@ -39,25 +34,21 @@ class _TopPageState extends State<TopPage> with SingleTickerProviderStateMixin {
             maxLines: 3,
           ),
           _buildCube(),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Center(
-                child: FloatingActionButton(
-                  elevation: 0,
-                  highlightElevation: 0,
-                  hoverElevation: 0,
-                  child: const Icon(
-                    Icons.keyboard_arrow_up_rounded,
-                    size: 30,
-                  ),
-                  onPressed: () {
-                    _openBottomSheet();
-                  },
-                ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            padding: const EdgeInsets.only(bottom: 32),
+            child: FloatingActionButton(
+              elevation: 0,
+              highlightElevation: 0,
+              hoverElevation: 0,
+              child: const Icon(
+                Icons.keyboard_arrow_up_rounded,
+                size: 36,
               ),
-              const Gap(36)
-            ],
+              onPressed: () {
+                _openBottomSheet();
+              },
+            ),
           ),
         ],
       ),
@@ -95,9 +86,6 @@ class _TopPageState extends State<TopPage> with SingleTickerProviderStateMixin {
       barrierColor: Colors.black.withOpacity(0.4),
       isScrollControlled: true,
       showDragHandle: true,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(32)),
-      ),
       builder: (context) {
         return DraggableScrollableSheet(
           expand: false,
@@ -107,136 +95,11 @@ class _TopPageState extends State<TopPage> with SingleTickerProviderStateMixin {
           builder: (context, scrollController) {
             return SingleChildScrollView(
               controller: scrollController,
-              child: _buildContents(),
+              child: const Contents(),
             );
           },
         );
       },
-    );
-  }
-
-  Widget _buildContents() {
-    return Container(
-      margin: const EdgeInsets.all(32),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'teshi04',
-                    style: TextStyle(fontSize: 32),
-                  ),
-                  Text(
-                    'Yui Matsuura',
-                    style: TextStyle(fontSize: 20, color: Colors.grey[600]),
-                  ),
-                ],
-              ),
-              CircleAvatar(
-                backgroundColor: AppColors.primaryColor,
-                radius: 64,
-                child: SvgPicture.asset(
-                  'assets/nekouo.svg',
-                ),
-              )
-            ],
-          ),
-          const Gap(16),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: items
-                .map(
-                  (item) => Padding(
-                    padding: const EdgeInsets.all(12),
-                    child: Tooltip(
-                      message: item['url'],
-                      margin: const EdgeInsets.only(left: 36),
-                      verticalOffset: 11,
-                      child: FilledButton.tonal(
-                        style: FilledButton.styleFrom(
-                          elevation: 0,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(32),
-                          ),
-                          padding: const EdgeInsets.all(16),
-                        ),
-                        onPressed: () async {
-                          await launchUrl(Uri.parse(item['url'] ?? ''));
-                        },
-                        child: Row(
-                          children: [
-                            Text(
-                              item['emoji'] ?? '',
-                              style: const TextStyle(
-                                  fontSize: 18, fontFamily: 'NotoColorEmoji'),
-                            ),
-                            const SizedBox(
-                              width: 8,
-                            ),
-                            Text(
-                              item['title'] ?? '',
-                              style: const TextStyle(
-                                fontSize: 18,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                )
-                .toList(),
-          ),
-          const Gap(32),
-          Row(
-            children: [
-              _buildCard(),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildCard() {
-    const suzuriUrl = 'https://suzuri.jp/teshi04';
-    return Card(
-      clipBehavior: Clip.hardEdge,
-      child: Stack(
-        children: [
-          Tooltip(
-            message: suzuriUrl,
-            child: InkWell(
-              child: Ink.image(
-                width: 160,
-                height: 160,
-                fit: BoxFit.cover,
-                image: const AssetImage('assets/suzuri.png'),
-                child: InkWell(
-                  onTap: () async {
-                    await launchUrl(Uri.parse(suzuriUrl));
-                  },
-                ),
-              ),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.all(16),
-            child: const Text(
-              'ウサ木',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-                fontFamily: 'MPLUSRounded1c',
-              ),
-            ),
-          )
-        ],
-      ),
     );
   }
 
